6d7303ef76b8ffde3c9ea4bfc2a2b0002115e67b
lzh <1216083447@qq.com>
2015-07-06 21:30:52 +0800

    keystone for openstack kilo

---
+++ Dockerfile
@@ -0,0 +1,46 @@
+# image name lzh/keystone:kilo
+FROM lzh/openstackbase:kilo
+
+MAINTAINER Zuhui Liu penguin_tux@live.com
+
+ENV BASE_VERSION 2015-07-01
+ENV OPENSTACK_VERSION kilo
+
+
+ENV DEBIAN_FRONTEND noninteractive
+
+RUN apt-get update
+RUN apt-get dist-upgrade -y
+RUN apt-get -t jessie-backports install keystone -y
+RUN apt-get install apache2 libapache2-mod-wsgi memcached python-memcache curl -y
+RUN apt-get clean
+
+RUN env --unset=DEBIAN_FRONTEND
+
+RUN cp -rp /etc/keystone/ /keystone
+RUN rm -rf /etc/keystone/*
+RUN rm -rf /var/log/keystone/*
+
+VOLUME ["/etc/keystone"]
+VOLUME ["/var/log/keystone"]
+
+ADD wsgi-keystone.conf  /etc/apache2/sites-available/wsgi-keystone.conf
+RUN ln -s /etc/apache2/sites-available/wsgi-keystone.conf /etc/apache2/sites-enabled
+
+RUN mkdir -p /var/www/cgi-bin/keystone
+
+RUN curl http://git.openstack.org/cgit/openstack/keystone/plain/httpd/keystone.py?h=stable/kilo \
+  | tee /var/www/cgi-bin/keystone/main /var/www/cgi-bin/keystone/admin
+
+RUN chown -R keystone:keystone /var/www/cgi-bin/keystone
+RUN chmod 755 /var/www/cgi-bin/keystone/*
+
+ADD apache2.conf /etc/supervisor/conf.d/apache2.conf
+ADD memcached.conf /etc/supervisor/conf.d/memcached.conf
+
+ADD entrypoint.sh /usr/bin/entrypoint.sh
+RUN chmod +x /usr/bin/entrypoint.sh
+
+EXPOSE 5000 35357
+
+ENTRYPOINT ["/usr/bin/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#set -e表示一旦脚本中有命令的返回值为非0，则脚本立即退出，后续命令不再执行;
+#set -o pipefail表示在管道连接的命令序列中，只要有任何一个命令返回非0值，则整个管道返回非0值，即使最后一个命令返回0.
+
+if [ -z "$KEYSTONE_DBPASS" ];then
+  echo "error: KEYSTONE_DBPASS not set"
+  exit 1
+fi
+
+if [ -z "$KEYSTONE_DB" ];then
+  echo "error: KEYSTONE_DB not set"
+  exit 1
+fi
+
+CRUDINI='/usr/bin/crudini'
+
+ADMIN_TOKEN=${ADMIN_TOKEN:=`openssl rand -hex 10`}
+CONNECTION=mysql://keystone:${KEYSTONE_DBPASS}@${KEYSTONE_DB}/keystone
+
+if [ ! -f /etc/keystone/.complete ];then
+    cp -rp /keystone/* /etc/keystone
+    touch /var/log/keystone/keystone.log
+    chown keystone:keystone /var/log/keystone/keystone.log
+    
+    $CRUDINI --set /etc/keystone/keystone.conf DEFAULT admin_token $ADMIN_TOKEN
+    $CRUDINI --set /etc/keystone/keystone.conf DEFAULT verbose True
+    $CRUDINI --set /etc/keystone/keystone.conf database connection $CONNECTION
+    $CRUDINI --set /etc/keystone/keystone.conf memcache servers localhost:11211
+    $CRUDINI --set /etc/keystone/keystone.conf token provider keystone.token.providers.uuid.Provider
+    $CRUDINI --set /etc/keystone/keystone.conf token driver keystone.token.persistence.backends.memcache.Token
+    $CRUDINI --set /etc/keystone/keystone.conf revoke driver keystone.contrib.revoke.backends.sql.Revoke
+    
+    touch /etc/keystone/.complete
+fi
+
+# 同步数据库
+echo 'select * from user limit 1;' | mysql -h$KEYSTONE_DB  -ukeystone -p$KEYSTONE_DBPASS keystone
+if [ $? != 0 ];then
+    su -s /bin/sh -c "keystone-manage db_sync" keystone
+fi
+
+/usr/bin/supervisord -n
